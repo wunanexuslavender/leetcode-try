0159571ec4f8206a1ddbfd368ea17e6594a388b6
wunanexuslavender <nexuslavender@gmail.com>
2026-09-22 21:58:48 +0800

    i hate network i have no idea how to connect my github without restart

--- 9月22.py
+++ 9月22.py
@@ -1,6 +1,8 @@
 #给定两个字符串 s 和 t ，它们只包含小写字母。
 #字符串 t 由字符串 s 随机重排，然后在随机位置添加一个字母
 #请找出在 t 中被添加的字母。
+#i hate network
+#i have no idea how to connect my github without restart
 class Solution:
 
     def findTheDifference(self, s: str, t: str) -> str:
